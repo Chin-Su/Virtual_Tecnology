af0a76577933855a3c67f5545ff697ba786cb5c8
Chin-Su <viet129@hotmail.com>
2023-07-20 14:38:05 +0700

    Upload project simulator simple restaurant use virtual toolkit by Unity

---
+++ Assets/Scripts/TurnLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class TurnLight : MonoBehaviour
+{
+    public bool check = true;
+
+    public GameObject[] lightPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+            check = !check;
+        foreach (var t in lightPos)
+            t.SetActive(check);
+    }
+}
